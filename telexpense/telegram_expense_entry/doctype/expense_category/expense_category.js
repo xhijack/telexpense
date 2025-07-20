f5e5c5f65ef0aa1647fdca3486fbbca8053b540b
Ramdani <xhijack@gmail.com>
2025-07-20 17:32:20 +0700

    ad expense category

---
+++ telexpense/telegram_expense_entry/doctype/expense_category/expense_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, PT Sopwer Teknologi Indonesia and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Expense Category", {
+// 	refresh(frm) {
+
+// 	},
+// });
